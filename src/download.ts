423520f654c3bba914fa37b2c3d03b2d65dac00a
peps1 <peps1@users.noreply.github.com>
2021-01-10 23:13:02 -0800

    allow for auto priority on download

--- src/download.ts
+++ src/download.ts
@@ -1,13 +1,14 @@
 import { printEvent } from './log';
 import { removeSearchAfterQueuing } from './queue';
+import { toApiPriority } from './utils';
 
 export const startDownload = async ( item: any, pos: number, instance: any, searchInfo: any, results: any, ) => {
   const result = results[0];
 
   if (result) {
     try {
-      global.SOCKET.post(`search/${instance.id}/results/${result.id}/download`, {
-        priority: item.priority,
+      await global.SOCKET.post(`search/${instance.id}/results/${result.id}/download`, {
+        priority: toApiPriority(item.priority),
         target_directory: item.target_directory,
       });
       if (global.SETTINGS.getValue('search_items')[pos].remove_after_found) {
