df59bffe5f8ca00fdc5bcbe73dcbfdacc8bfdb1e
peps1 <peps1@users.noreply.github.com>
2021-09-06 17:58:23 -0700

    adding more types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,9 +1,32 @@
 export interface SearchHistory {
-  name: string,
+  pattern: string,
+  patternIndex: number,
   timestamp: Date,
-  listid: number
+  searchItemId: number
 }
 
 export interface DBData {
   search_history: SearchHistory[]
+}
+
+export interface SearchPatternItem {
+  searchItemId: number,
+  patternIndex: number,
+  singlePattern: string
+}
+
+export interface SearchItem {
+  pattern_list: string,
+  excluded: string,
+  excluded_users: string,
+  extensions: string,
+  file_type: string,
+  min_size: number,
+  remove_after_found: boolean,
+  exact_match: boolean,
+  queue_all: boolean,
+  queue_dupe: string,
+  remove_dupe: boolean,
+  priority: number,
+  target_directory: string
 }
