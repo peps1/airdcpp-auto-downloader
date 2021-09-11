9fd76aff5eefe9b428c752d9c562cc1b0640f0be
peps1 <peps1@users.noreply.github.com>
2021-09-10 22:18:15 -0700

    fix type for SOCKET

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,11 +1,13 @@
 /* eslint-disable no-var */
 
+type APISocket = import('airdcpp-apisocket').APISocket;
+
 // type DBData = import('./types').DBData;
 // type Low = import('lowdb/lib').Low<DBData>;
 
 declare var EXTENSION: any;
 // declare var DB: Low;
 declare var SETTINGS: any;
-declare var SOCKET: any;
+declare var SOCKET: APISocket;
 declare var SEARCH_INTERVAL: ReturnType<typeof setInterval>;
 declare var EXTENSION_NAME: string;
