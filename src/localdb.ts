2530f98fa40d4a7d804b2fb708f4ad0e6d9450e3
peps1 <peps1@users.noreply.github.com>
2021-10-10 19:40:15 -0700

    add try/catch when loading db file

--- src/localdb.ts
+++ src/localdb.ts
@@ -3,13 +3,20 @@ import StormDB from 'stormdb';
 
 export const getDb = async (dbFilePath: string) => {
 
-  const engine = new StormDB.localFileEngine(dbFilePath, {async: true});
-  const dbObj = new StormDB(engine);
+  try {
+    const engine = new StormDB.localFileEngine(dbFilePath, {async: true});
+    const dbObj = new StormDB(engine);
+    dbObj.default({
+      search_history:  [],
+    });
 
-  dbObj.default({
-    search_history:  [],
-  });
+    return dbObj;
+  } catch (e) {
+    global.SOCKET.logger.error('ERROR: Problem loading db file');
+    throw (e);
+  }
 
-  return dbObj;
+
+
 
 };
